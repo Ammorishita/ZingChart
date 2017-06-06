2ca0419470e42762d4d184f371c221096c57fb22
Alex <ammorishita@gmail.com>
2017-06-05 20:32:00 -0700

    Add basic graph styling of labels and objects. Implement update on series values with each search.

--- js/chart.js
+++ js/chart.js
@@ -1,30 +1,133 @@
-let chartData = {
-	type: 'bar',
-	title: {
-		text: "Yelp Restaurant Comparisons"
+var currentStars = yelpData[0];
+var currentReviews = yelpData[1];
+var currentName;
+var currentURL = yelpData[3];
+var chartData = {
+	'type':'bar',
+	'title':{
+		'text': currentName,
+		'z-index':'1000',
+		'margin-top':'20px',
+		'padding':'45 45',
 	},
-	plot: {
-		'animation': {
-			'effect': '4',
-			'method': '0',
-			'sequence': '0',
-			'speed': '5'
+	'plot':{
+		'animation':{
+			'effect':'4',
+			'method':'0',
+			'sequence':'0',
+			'speed':'5'
 		}
 	},
-	legend: {},
-	series: [
+	'plotarea':{
+
+	},
+	'labels':[
+       {
+			'id':'label1',
+			'text':'Visit Yelp Page',
+			'x':400,
+			'y':50,
+			'background-color':'red',
+			'color':'white',
+			'padding':5,
+			'url':currentURL,
+			'target':'_blank'
+       }
+    ],
+	'legend':{
+		'layout':'1x2',
+		'align':'center',
+		'vertical-align':'top',
+		'item':{
+			'font-size':'18',
+		}
+	},
+	'scale-x':{
+    	'labels':['Results'],
+    	'item':{
+    		'font-color':'Black',
+    		'font-size':'16'
+    	}
+    },
+	'series' : [
 		{
-			values: [1,5,10,50,51],
-			'styles': ['red','blue','orange','yellow','purple']
+			'values':[currentStars],
+			'text':'Rating',
+			'scales':'scale-x, scale-y',
+			'legend-marker':{
+				'background-image':'images/rating.png',
+				'background-fit':'xy',
+				'size':'16px',
+				'background-color':'none'
+			},
+			'legend-item':{
+				'color':'Dodgerblue'
+			}
 		},
 		{
-			values: [2,3,4,5]
+			'values':[currentReviews],
+			'text':'Reviews',
+			'scales':'scale-x, scale-y-2',
+			'legend-marker':{
+				'background-image':'images/reviews.svg',
+				'background-fit':'xy',
+				'size':'16px',
+				'background-color':'none'
+			},
+			'legend-item':{
+				'color':'Red'
+			}
 		}
 	],
+	'scale-y': {
+		'values':'0:5:1',
+		'line-color':'Dodgerblue',
+		'line-width':'3',
+		'item':{
+    		'font-color':'DarkBlue',
+    		'font-size':'16'
+    	}
+
+	},
+	'scale-y-2':{
+		'values':'0:1000:100',
+		'line-color':'Red',
+		'line-width':'3',
+		'item':{
+    		'font-color':'DarkRed',
+    		'font-size':'14'
+    	}
+	}
 };
 zingchart.render({
 	id: 'chartDiv',
 	data: chartData,
 	height: 400,
-	width: 500
+	width: 500,
 });
+function renderChart() {
+	currentStars = yelpData[0];
+	currentReviews = yelpData[1];
+	currentName = yelpData[2];
+	currentURL = yelpData[3];
+	zingchart.exec('chartDiv', 'setseriesvalues', {
+	    values : [
+	        [currentStars],
+	        [currentReviews]
+	    ]
+	});
+	zingchart.exec('chartDiv', 'modify', {
+	    graphid : 0,
+	    object : 'title',
+	    data : {
+	        text : currentName
+	    }
+	});
+	zingchart.exec('chartDiv', 'updateobject', {
+	    'type' : 'label',
+	    'data' : {
+	        'id' : 'label1',
+	        'url' : currentURL
+	    }
+	});
+};
--- js/script.js
+++ js/script.js
@@ -1,16 +1,28 @@
-let engine = {
+var addChart = false;
+var searchTerm = ' ';
+var yelpData = [];
+var engine = {
 	init: function() {
 		this.yelpAPI();
+		this.search();
+	},
+	search: function() {
+		let button = document.getElementById('search');
+		button.addEventListener('keydown', function(e) {
+			if(e.keyCode === 13 ) {
+				searchTerm = button.value;
+				engine.yelpAPI();
+			}
+		});					
 	},
 	yelpAPI: function(){
 		function nonce_generate() {
   			return (Math.floor(Math.random() * 1e12).toString());
-		}
+		};
 		var YELP_BASE_URL = 'https://api.yelp.com/v2/search/';
 		var YELP_KEY_SECRET = 'YJgc-BtCt9ogrrDXz5ptbkFJ3mo';
 		var YELP_TOKEN_SECRET = 'gK1IB6E7txoJUpH3AK7p52iF8MQ';
 		var yelp_url = YELP_BASE_URL;
-
 	    var parameters = {
 	      oauth_consumer_key: 'REfQ41eJe5W6cOXEKbedIw',
 	      oauth_token: 'yXeLtJ768XlAbLUoLwPJAnRqCSZv5MI_',
@@ -20,7 +32,7 @@ let engine = {
 	      oauth_version : '1.0',
 	      callback: 'cb', 
 	      limit: 1,
-	      term: 'manna+bbq',
+	      term: searchTerm,
 	      location: 'San+Diego'
 	    };
     	var encodedSignature = oauthSignature.generate('GET',yelp_url, parameters, YELP_KEY_SECRET, YELP_TOKEN_SECRET);
@@ -34,14 +46,26 @@ let engine = {
 	    };
 
 	    $.ajax(settings).done(function(results){
-	    	console.log(results);
-	    	var yelpID = results.businesses[0].id;
-	    	var yelpImage = results.businesses[0].image_url;
-	    	var yelpSnippet = results.businesses[0].snippet_text;
+	    	if(results.businesses.length < 1) {
+	    		alert('No results found')
+	    	}
+	    	console.log(results)
+	    	var yelpName = results.businesses[0].name;
+	    	var yelpRating = results.businesses[0].rating;
+	    	var yelpReviewCount = results.businesses[0].review_count;
 	    	var yelpURL = results.businesses[0].url;
-	    	let script = document.createElement('script');
-	    	script.src = 'js/chart.js';
-	    	document.getElementsByTagName('body')[0].appendChild(script);		    	
+	    	yelpData = [];
+	    	yelpData.push.apply(yelpData, [yelpRating,yelpReviewCount,yelpName,yelpURL]);
+	    	renderChart();
+	    	console.log(yelpData[2]);
+	    	if(!addChart) {
+		    	let script = document.createElement('script');
+		    	script.src = 'js/chart.js';
+		    	document.getElementsByTagName('body')[0].appendChild(script);		    	
+				addChart = true;    		
+	    	} else {
+	    		return;
+	    	}
 	    }).fail(function(){
 	    	new Error();
 	    	console.log('we failed to get results')
